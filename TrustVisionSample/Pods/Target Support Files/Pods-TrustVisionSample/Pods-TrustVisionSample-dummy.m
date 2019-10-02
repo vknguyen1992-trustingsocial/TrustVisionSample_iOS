bd5a1b96276f59b4d582be764b502a37e3f11e9e
vknguyen1992 <nguyen.vu@trustingsocial.com>
2019-10-02 17:57:59 +0700

    add .gitignore

--- TrustVisionSample/Pods/Target Support Files/Pods-TrustVisionSample/Pods-TrustVisionSample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TrustVisionSample : NSObject
-@end
-@implementation PodsDummy_Pods_TrustVisionSample
-@end
--- TrustVisionSample/Pods/Target Support Files/Pods-TrustVisionSampleUITests/Pods-TrustVisionSampleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TrustVisionSampleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TrustVisionSampleUITests
-@end
